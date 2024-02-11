a6c306371d2387bfd16a6221742e8c362cc24012
Serj Gabyan <niteip197@gmail.com>
2024-02-11 18:35:10 +0500

    Updates

--- loading.js
+++ loading.js
@@ -8,17 +8,36 @@ function loadingOff(){
   let loader = document.getElementById('loaderHolder');
   loader.style.opacity = '0';
   setTimeout(() => {
-    let background = document.getElementById('backgroundHolder');
-    background.style.opacity = '1';
     loader.style.zIndex = '-50';
+    let aboutButton = document.getElementById('aboutButton');
+    let portfolioButton = document.getElementById('portfolioButton');
+    let servicesButton = document.getElementById('servicesButton');
+    let contactsButton = document.getElementById('contactsButton');
+    let navigation = document.getElementById('navigationHolder');
+    navigation.style.opacity = '1';
+    aboutButton.classList.add('revealAbout');
+    portfolioButton.classList.add('revealPortfolio');
+    servicesButton.classList.add('revealServices');
+    contactsButton.classList.add('revealContacts');
     setTimeout(() => {
-      let lines = document.getElementById('linesHolder');
-      lines.style.opacity = '1';
-    }, 500);
+      aboutButton.classList.remove('revealAbout');
+      portfolioButton.classList.remove('revealPortfolio');
+      portfolioButton.style.opacity = '1';
+      servicesButton.classList.remove('revealServices');
+      servicesButton.style.opacity = '1';
+      contactsButton.classList.remove('revealContacts');
+      contactsButton.style.opacity = '1';
+    }, 3700);
     setTimeout(() => {
-      let navigation = document.getElementById('navigationHolder');
-      navigation.style.opacity = '1';
-    }, 1000);
+      let lines = document.getElementById('mainLines');
+      let maskLines = document.getElementById('maskLines');
+      let logo = document.getElementById('logo');
+      logo.style.opacity = '1';
+      lines.classList.add('lineReveil');
+      setTimeout(() => {
+        maskLines.style.opacity = '1';
+      }, 700);
+    }, 3000);
     siteLoaded();
   }, 1000);
   return;
