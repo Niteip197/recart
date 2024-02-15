f8832392fb1de98c926dfcd1f9c0c0e217535767
Serj Gabyan <niteip197@gmail.com>
2024-02-15 17:55:20 +0500

    Updates

--- loading.js
+++ loading.js
@@ -36,13 +36,49 @@ function loadingOff(){
       lines.classList.add('lineReveil');
       setTimeout(() => {
         maskLines.style.opacity = '1';
+        setTimeout(() => {
+          introStart();
+        }, 500);
       }, 700);
     }, 3000);
-    siteLoaded();
   }, 1000);
   return;
 }
 
-function siteLoaded(){
-  return;
+function introStart(){
+  let introText1 = document.getElementById('introT1');
+  let introText2 = document.getElementById('introT2');
+  let introText3 = document.getElementById('introT3');
+  let introText4 = document.getElementById('introT4');
+  introText1.style.left = '600px';
+  setTimeout(() => {
+    introText1.style.transform = 'scale(80%) translateX(41.25px) translateY(-35px)';
+    introText1.style.opacity = '0.8';
+    introText2.style.bottom = '170px'
+    setTimeout(() => {
+      introText1.style.transform = 'scale(70%) translateX(88.35px) translateY(-77px)';
+      introText1.style.opacity = '0.6';
+      introText2.style.transform = 'scale(80%) translateX(41.25px) translateY(-35px)';
+      introText2.style.opacity = '0.8';
+      introText3.style.bottom = '170px'
+      setTimeout(() => {
+        introText1.style.transform = 'scale(60%) translateX(143.35px) translateY(-126px)';
+        introText1.style.opacity = '0.4';
+        introText2.style.transform = 'scale(70%) translateX(88.35px) translateY(-77px)';
+        introText2.style.opacity = '0.5';
+        introText3.style.transform = 'scale(80%) translateX(41.25px) translateY(-35px)';
+        introText3.style.opacity = '0.6';
+        introText4.style.bottom = '170px';
+        setTimeout(() => {
+          introText1.style.transform = 'scale(60%) translateX(1950px) translateY(-126px)';
+          introText1.style.transition = 'all 2s cubic-bezier(.79,0,.76,.79)';
+          introText2.style.transform = 'scale(70%) translateX(1850px) translateY(-77px)';
+          introText2.style.transition = 'all 2s cubic-bezier(.79,0,.76,.79)';
+          introText3.style.transform = 'scale(80%) translateX(1850px) translateY(-35px)';
+          introText3.style.transition = 'all 2s cubic-bezier(.79,0,.76,.79)';
+        }, 2000);
+      }, 2500);
+    }, 2500);
+  }, 2500);
+  return
 }
